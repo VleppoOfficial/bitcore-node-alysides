7b4bf99a0794b18916810da5b318fd0497aaf302
danssolutions <32322781+danssolutions@users.noreply.github.com>
2023-03-06 11:26:04 +0200

    komodo -> alysides

--- regtest/node.js
+++ regtest/node.js
@@ -9,7 +9,7 @@ var log = index.log;
 log.debug = function() {};
 
 var chai = require('chai');
-var bitcore = require('bitcore-lib-komodo');
+var bitcore = require('bitcore-lib-alysides');
 var rimraf = require('rimraf');
 var node;
 
